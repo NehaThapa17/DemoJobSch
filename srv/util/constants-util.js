99a504bbb7703ee81f75a25e6ffe999e81d01502
NThapa@marathonpetroleum.com <NThapa@marathonpetroleum.com>
2022-12-01 13:20:45 +0000

    const Dest name

--- srv/util/constants-util.js
+++ srv/util/constants-util.js
@@ -1,5 +1,5 @@
 module.exports=Object.freeze({
-    // DESTINATIONNAME:"ds4rt",
+    // DESTINATIONNAME:"s4rt",
     DESTINATIONNAME:"ds4rt_120",
     CREATEPRODUCTURL:"sap/opu/odata/sap/ZHSC_PRICING_NOTIF_SRV",
     CREATETERMINALURL:"sap/opu/odata/sap/ZHSC_PRICING_NOTIF_SRV" 
